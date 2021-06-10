74989d0cadfaed834984b7e45328a29b5fb1b485
AminCht <amincht81@gmail.com>
2021-06-11 00:54:39 +0430

    'main'

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
-	// wlk;lk;lk;kl
-        // Alllloooooo
-        ///FOADDDDDDDDDD
-        //jhkjhkjh
-        //lll
+	    launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader=new FXMLLoader(this.getClass().getResource("view/Othello.fxml"));
+        loader.load();
+        primaryStage.setScene(new Scene(loader.getRoot()));
+        primaryStage.show();
     }
 }

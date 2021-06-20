1eca48afc4f7030fef7e27182745a4c9822fc341
foad <foad1381@gmail.com>
2021-06-21 02:35:36 +0430

    label

--- src/controller/Logincontroller.java
+++ src/controller/Logincontroller.java
@@ -3,6 +3,7 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -26,11 +27,15 @@ public class Logincontroller implements Initializable {
     private TextField player2user;
     @FXML
     private Button playbtn;
+    @FXML
+    private Label startError;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         playbtn.setOnAction(e->{
             if(!(player1user.getText().isEmpty()||player2user.getText().isEmpty())) {
                 if(!(player1user.getText().equals(player2user.getText()))) {
+                    startError.setText("");
+                    startError.setVisible(false);
                     Player player1 = new Player(player1user.getText(), 2);
                     Player player2 = new Player(player2user.getText(), 2);
                     othelloController1.makebuttonsselectable();
@@ -38,6 +43,15 @@ public class Logincontroller implements Initializable {
                     othelloController1.resetGame();
                     stage.close();
                 }
+            }
+            else{if(player1user.getText().equals(player2user.getText())){
+                startError.setText("player names cant be same");
+                startError.setVisible(true);
+            }
+            else {if(player1user.getText().isEmpty()||player2user.getText().isEmpty()){
+                startError.setText("set name for both players");
+                startError.setVisible(true);
+            }}
             }
         });
     }

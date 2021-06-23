d0d983fdc27d767269b3229f1a1fe16221984f78
foad <foad1381@gmail.com>
2021-06-23 16:41:40 +0430

    final

--- src/controller/TableController.java
+++ src/controller/TableController.java
@@ -11,7 +11,6 @@ import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.ResourceBundle;
 
 public class TableController implements Initializable {
